3bee1111e94a47b2911448a3613369e8628cdf20
Avins VR <avins2005@gmail.com>
2025-06-24 15:45:48 +0530

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  basePath: '/Foodies',
+  assetPrefix: '/Foodies/',
+};
 
 export default nextConfig;
