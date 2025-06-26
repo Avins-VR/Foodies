5562ba8a7852e842b3d651f203ebba4e09e5aac1
Avins VR <avins2005@gmail.com>
2025-06-26 12:43:07 +0530

    Fix: removed basePath and assetPrefix for Vercel

--- next.config.js
+++ next.config.js
@@ -1,12 +1,8 @@
-const isProd = process.env.NODE_ENV === 'production';
-
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   images: {
     unoptimized: true,
   },
-  basePath: isProd ? '/Foodies' : '',
-  assetPrefix: isProd ? '/Foodies/' : '',
   trailingSlash: true,
 };
 
